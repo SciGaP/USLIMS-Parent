41dcbf0f58b8d70b9132af431042201193eb6bfe
root <root@9b78175e-3458-45e4-9b4b-1be6c25a9a53>
2012-09-16 13:27:08 +0000

    updated -b.d-
    git-svn-id: svn://bcf2.uthscsa.edu/uslims3_webinfo/trunk@8 9b78175e-3458-45e4-9b4b-1be6c25a9a53

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+/*  Database and other configuration information - Required!!  
+ -- Configure the Variables Below --
+
+*/
+
+$org_name           = 'UltraScan III LIMS';
+$org_site           = 'uslims3.uthscsa.edu';
+
+$dbusername         = 'new_us3user';  # the name of the MySQL user
+$dbpasswd           = 'Sv3dberg';  # the password for the MySQL user
+$dbname             = 'newus3';  # the name of the database
+$dbhost             = 'uslims3.uthscsa.edu'; # the host on which MySQL runs, generally localhost
+?>
