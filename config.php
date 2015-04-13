bba0a6d55dc788504c800af023e7ce48bb162c9f
gegorbet <gegorbet@9b78175e-3458-45e4-9b4b-1be6c25a9a53>
2015-04-13 21:31:05 +0000

    minor fix to starting config.php
    git-svn-id: svn://bcf2.uthscsa.edu/uslims3_webinfo/trunk@10 9b78175e-3458-45e4-9b4b-1be6c25a9a53

--- config.php
+++ config.php
@@ -10,5 +10,5 @@ $org_site           = 'uslims3.uthscsa.edu';
 $dbusername         = 'new_us3user';  # the name of the MySQL user
 $dbpasswd           = 'Sv3dberg';  # the password for the MySQL user
 $dbname             = 'newus3';  # the name of the database
-$dbhost             = 'uslims3.uthscsa.edu'; # the host on which MySQL runs, generally localhost
+$dbhost             = 'localhost'; # the host on which MySQL runs, generally localhost
 ?>
